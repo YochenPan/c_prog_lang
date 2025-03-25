6f822f6d8bff154fe596c0356cf3f902a19a2c40
YochenPan <yoyo080520@yahoo.com.tw>
2025-03-25 16:07:27 +0800

    Update f2.c

--- 04/f2.c
+++ 04/f2.c
@@ -16,5 +16,14 @@ int main()
   printf("a == b :%d\n", a == b);
   printf("a != b :%d\n", a != b);
 
+  printf("a > 1 && b < 3 :%d\n",a > 1 && b < 3);
+  printf("a > 1 || b < 3 :%d\n",a > 1 || b < 3);
+ 
+  printf("a > 1 && a < 3 :%d\n",a > 1 && a < 3);
+  printf("b > 1 && b < 3 :%d\n",b > 1 && b < 3);
+
+  printf("a > 1 || a < 3 :%d\n",a > 1 || a < 3);
+  printf("b > 1 || b < 3 :%d\n",b > 1 || b < 3);
+  
   return 0;
 }
